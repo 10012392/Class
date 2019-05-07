65a7d0119a1f2f3ee69df2f4ab6819026e19be09
Lara Corkery <10012392@student.toiohomai.ac.nz>
2019-05-08 11:44:27 +1200

    Saving Files

---
+++ ClassTasks/ClassTasks/Program.cs
@@ -0,0 +1,27 @@
+﻿//Lara Corkery 08/05/2019
+//Class Tasks 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car c1 = new Car();
+            c1.Make = " ";
+            c1.Model = " ";
+            c1.Color = " ";
+            c1.Rego = " ";
+        }
+
+        public class Car
+        {
+
+        }
+    }
+}
